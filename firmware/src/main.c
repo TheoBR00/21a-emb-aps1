afe75d047c68edb01319f72245aa150ff8a0a35f
Rafael Corsi <corsiferrao@gmail.com>
2021-03-08 16:37:45 -0300

    update example

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -4,23 +4,20 @@
 #include "gfx_mono_text.h"
 #include "sysfont.h"
 
-void init (void) {
+int main (void)
+{
 	board_init();
 	sysclk_init();
 	delay_init();
+
+  // Init OLED
 	gfx_mono_ssd1306_init();
-}
-
-int main (void)
-{
-
-  init();
-  /* Insert application code here, after the board has been initialized. */
-
+  
   // Escreve na tela um circulo e um texto
 	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
   gfx_mono_draw_string("mundo", 50,16, &sysfont);
 
+  /* Insert application code here, after the board has been initialized. */
 	while(1) {
 
 	}
